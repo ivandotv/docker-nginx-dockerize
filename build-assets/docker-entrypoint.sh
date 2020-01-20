c6295564fa17422bbb694d6182042a5900ba9708
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-01-20 21:53:09 +0100

    add convenient status message

--- build-assets/docker-entrypoint.sh
+++ build-assets/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "entry point"
 #process main conf file
 dockerize  -template /etc/nginx/nginx.conf:/etc/nginx/nginx.conf
 
@@ -9,6 +8,7 @@ dockerize  -template /etc/nginx/conf.d:/etc/nginx/conf.d/
 
 #process all files in custom template directory
 if [[ -d /etc/nginx/custom.tmpl.d ]];then
+    echo 'processing /etc/nginx/custom.tmpl.d'
     dockerize  -template /etc/nginx/custom.tmpl.d:/etc/nginx/custom.d/
 fi
 
